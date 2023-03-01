7350d7a06153f3b7ab34ba07e1abc49c5db8e369
Fabrizio Feitosa <fabriziofeitosa@live.com>
2023-03-01 11:53:53 -0300

    Ajustes + repositório

---
+++ script.js
@@ -0,0 +1,7 @@
+function streamVideo() {
+    const video = document.querySelector("video");
+    const options = { video: true };
+    navigator.mediaDevices.getUserMedia(options)
+        .then( stream => video.srcObject = stream)
+        .catch( err => console.error(err));
+}
