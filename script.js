ea409264a1f54331dd98090ad4d0bb3d1a4e8b41
Fabrizio Feitosa <fabriziofeitosa@live.com>
2023-03-01 12:10:47 -0300

    encapsulamento

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function streamVideo() {
-    const video = document.querySelector("video");
-    const options = { video: true };
-    navigator.mediaDevices.getUserMedia(options)
-        .then( stream => video.srcObject = stream)
-        .catch( err => console.error(err));
-}
+(function() {
+    function streamVideo() {
+        const video = document.querySelector("video");
+        const options = { video: true };
+        navigator.mediaDevices.getUserMedia(options)
+            .then( stream => video.srcObject = stream)
+            .catch( err => console.error(err));
+    }
+
+    document.querySelector('.mirror button')
+        .addEventListener('click', streamVideo);
+})();
